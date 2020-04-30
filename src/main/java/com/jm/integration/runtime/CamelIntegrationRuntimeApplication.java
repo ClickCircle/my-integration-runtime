ef728b8029b09a9a36349536e0548594afbaebd4
zhangjunmao <Qwerasdf0897~>
2020-04-30 14:53:24 +0800

    commit

--- src/main/java/com/jm/integration/runtime/CamelIntegrationRuntimeApplication.java
+++ src/main/java/com/jm/integration/runtime/CamelIntegrationRuntimeApplication.java
@@ -9,5 +9,4 @@ public class CamelIntegrationRuntimeApplication {
     public static void main(String[] args) {
         SpringApplication.run(CamelIntegrationRuntimeApplication.class, args);
     }
-
 }
